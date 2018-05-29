726e7edc3a87493e0f7ab7ebb54881971ef77622
harold.posadas <harold.posadas@itaas.dimensiondata.com>
2018-05-29 13:12:13 -0700

    update test-connect script - use staging vc

--- test-connect.ps1
+++ test-connect.ps1
@@ -2,6 +2,10 @@
 
 Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false
 
-connect-viserver -server 172.17.71.21 -WarningAction SilentlyContinue
+# Staging vcenter 172.16.168.21
+connect-viserver -server 172.16.168.21 -WarningAction SilentlyContinue
+
+# Prod vcenter - NA16 mgmt
+#connect-viserver -server 172.17.71.21 -WarningAction SilentlyContinue
 
 Get-VM
