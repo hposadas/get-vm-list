368891a6a50c73d85ffe727d5b89df8cac91e167
harold.posadas <harold.posadas@itaas.dimensiondata.com>
2018-05-29 13:03:33 -0700

    update files

--- get-creds-and-vm-list.ps1
+++ get-creds-and-vm-list.ps1
@@ -2,8 +2,9 @@ $VC_Servers_To_Scan = @('172.17.71.21')
 
 Write-Host "CONNECTING TO VCs, PROVIDE CREDENTIALS"
 
-$Creds = Get-Credential
-
+#$Creds = Get-Credential
+$USERNAME
+$PASSWORD
 
 #CONNECT TO VC SERVER
 
@@ -11,7 +12,8 @@ foreach($VIServer in $VC_Servers_To_Scan){
 
     Write-Host -ForegroundColor Green "CONNECTING TO $VIServer..."
 
-    $Blank = Connect-VIServer $VIServer -Credential $Creds -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+    #$Blank = Connect-VIServer $VIServer -Credential $Creds -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+    $Blank = Connect-VIServer $VIServer -User $USERNAME -Password $PASSWORD -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
 
     Get-VM -Name *
 }
--- test-connect.ps1
+++ test-connect.ps1
@@ -1,5 +1,7 @@
 #Set-PowerCLIConfiguration -InvalidCertificateAction Ignore
 
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false
+
 connect-viserver -server 172.17.71.21 -WarningAction SilentlyContinue
 
 Get-VM
