55ff59b6f61dff4664f764790e655f04e6210215
harold.posadas <harold.posadas@itaas.dimensiondata.com>
2018-05-29 13:59:33 -0700

    add credential parameters

--- test-connect.ps1
+++ test-connect.ps1
@@ -3,7 +3,8 @@
 Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false
 
 # Staging vcenter 172.16.168.21
-connect-viserver -server 172.16.168.21 -WarningAction SilentlyContinue
+#connect-viserver -server 172.16.168.21 -WarningAction SilentlyContinue
+connect-viserver -server 172.16.168.21 -User $USERNAME -Password $PASSWORD -WarningAction SilentlyContinue
 
 # Prod vcenter - NA16 mgmt
 #connect-viserver -server 172.17.71.21 -WarningAction SilentlyContinue
